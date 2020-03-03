e5cceeee654420042522c386b6c5280cb50871b9
Santiago Betancur <scb9908@gmail.com>
2020-03-03 16:01:14 +0100

    Iterative search

--- graph.py
+++ graph.py
@@ -1,8 +1,3 @@
-# Graph is an ordered pair or Arc-edges(E) and nodes-vertices(V) G = (V, E)
-# ordered pair (a, b) !== (b, a) if a !== b
-# unordered {a, b} == {b, a}
-# edges(arc) could be either directed or indirected
-# Breadth First Search is using queue
 from queue import Queue, PriorityQueue
 from functools import reduce
 import graphutils
@@ -16,14 +11,50 @@ class Arc:
 
 
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, heuristic=0):
+        self.heuristic = heuristic
         self.value = value
 
+    def __gt__(self, node):
+        return self.value > node.value
 
 class Graph:
     def __init__(self, nodes, arcs):
         self.nodes = graphutils.init_edged_list(nodes, arcs)
         self.nodes_length = len(nodes)
+
+    def a_star_search(self, initial_node, final):
+        previos_visited_nodes = graphutils.init_visited_nodes(self.nodes.keys())
+        priority_queue = PriorityQueue()
+        priority_queue.put((initial_node, initial_node.heuristic))
+
+        graph_score = {}
+        final_score = {}
+
+        for node in self.nodes.keys():
+            graph_score[node] = final_score[node] = 1e1000
+        
+        graph_score[initial_node.value] = 0
+        final_score[initial_node.value] = initial_node.heuristic
+
+        while not priority_queue.empty():
+            current = priority_queue.get()
+
+            if (current[0].value == final.value):
+                break
+            
+            for neighboor in self.neighboors(current[0]):
+                estimated_score = graph_score[current[0].value] + neighboor[1]
+
+                if estimated_score < graph_score[neighboor[0].value]:
+                    previos_visited_nodes[neighboor[0].value] = current
+                    graph_score[neighboor[0].value] = estimated_score
+                    final_score[neighboor[0].value] = graph_score[neighboor[0].value] + neighboor[0].heuristic
+
+                    if not any((neighboor[0].heuristic, neighboor[0]) in nodes for nodes in priority_queue.queue):
+                        priority_queue.put((neighboor[0], final_score[neighboor[0].value]))
+
+        return {'distance': final_score, 'previous': previos_visited_nodes}
 
     def deep_first_search(self, initial_node, callback):
         visited_nodes = graphutils.init_visited_nodes(self.nodes.keys())
@@ -62,56 +93,28 @@ class Graph:
 
         return graph_nodes
 
-    def dijkstra(self, initial_node):
-        visited_nodes = graphutils.init_visited_nodes(self.nodes.keys())
-        previous = graphutils.init_visited_nodes(self.nodes.keys())
-
-        # Dijkstra distance
-        distance = {}
-        for node in self.nodes.keys():
-            distance[node] = 1e1000
-
-        distance[initial_node.value] = 0
-        queue = Queue()
-        queue.put((initial_node, 0))
-
-        while not queue.empty():
-            current_node = queue.get()
-            visited_nodes[current_node[0].value] = True
-            for neighboor in self.neighboors(current_node[0]):
-                if visited_nodes[neighboor[0].value] == True:
-                    continue
-                new_distance = distance[current_node[0].value] + neighboor[1]
-                if new_distance < distance[neighboor[0].value]:
-                    previous[neighboor[0].value] = current_node
-                    distance[neighboor[0].value] = new_distance
-                    queue.put((neighboor[0], new_distance))
-
-        # print(previous)
-        return {'distance': distance, 'previous': previous}
-
     def find_path(self, initial_node, final_node):
-        dijkstra = self.dijkstra(initial_node)
+        search = self.a_star_search(initial_node, final_node)
         path = []
 
-        if (dijkstra.get('previous')[final_node.value] == False):
+        if (search.get('previous')[final_node.value] == False):
             return {'path': path, 'distance': 10e2000}
 
         queue = Queue()
-        queue.put(dijkstra.get('previous')[final_node.value])
+        queue.put(search.get('previous')[final_node.value])
 
         while not queue.empty():
             current = queue.get()
             if current is not False:
                 path.append(current)
-                queue.put(dijkstra.get('previous')[current[0].value])
-        # Put last node
+                queue.put(search.get('previous')[current[0].value])
+
         current_distance = reduce(lambda acumulated, current, : acumulated + current[1], path, 0)
-        path.insert(0, (final_node, abs(dijkstra.get('distance')[final_node.value] - current_distance)))
+        path.insert(0, (final_node, abs(search.get('distance')[final_node.value] - current_distance)))
 
         return {
             'path': list(reversed(path)),
-            'distance': dijkstra.get('distance')[final_node.value]
+            'distance': search.get('distance')[final_node.value]
         }
 
     def is_cyclic(self, initial_node):
@@ -121,14 +124,16 @@ class Graph:
         queue.put(initial_node)
 
         while not queue.empty():
-            current_node = queue.get()
-            unvisited_nodes[current_node.value] = True
+            current = queue.get()
+            unvisited_nodes[current.value] = True
+            neighboors = self.neighboors(current)
 
-            neighboors = self.neighboors(current_node)
             for neighboor in neighboors:
                 if unvisited_nodes[neighboor[0].value] == True:
                     return True
+
                 queue.put(neighboor[0])
+
         return False
 
     def is_bipartite(self, initial_node):
@@ -137,13 +142,32 @@ class Graph:
         queue.put(initial_node)
 
         while not queue.empty():
-            current_node = queue.get()
-            unvisited_nodes[current_node.value] = True
-            for neighboor in self.neighboors(current_node):
-                if unvisited_nodes[neighboor[0].value] == False:
+            current = queue.get()
+            unvisited_nodes[current.value] = True
+
+            for neighboor in self.neighboors(current):
+                if not unvisited_nodes[neighboor[0].value]:
                     queue.put(neighboor[0])
 
-        return len(list(filter(lambda visited: visited == False, unvisited_nodes.values()))) >= 1
+        return len(list(filter(lambda visited: not visited, unvisited_nodes.values()))) >= 1
+
+    def iterative_search(self, initial_node, goal_node, depth):
+        def limited_search(root, final_node, limit):
+            if (root.value == final_node.value):
+                return True
+
+            if (limit <= 0):
+                return False
+
+            for neighboor in self.neighboors(root):
+                if limited_search(neighboor[0], final_node, limit - 1):
+                    return True
+                return False
+
+        for i in range(depth):
+            if limited_search(initial_node, goal_node, i):
+                return True
+        return False
 
     def neighboors(self, node):
         return self.nodes[node.value]
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from tree import Tree
 from graph import Graph, Node, Arc
 
-node_mail = Node('Mail')
-node_ts = Node('ts')
-node_103 = Node('103')
-node_b3 = Node('b3')
-node_b1 = Node('b1')
-node_c2 = Node('c2')
-node_c1 = Node('c1')
-node_c3 = Node('c3')
-node_b2 = Node('b2')
-node_b4 = Node('b4')
-node_109 = Node('109')
-node_111 = Node('111')
-node_119 = Node('119')
-node_storage = Node('storage')
-node_123 = Node('123')
-node_123r = Node('123r')
-node_125 = Node('125')
+node_mail = Node('Mail', 0)
+node_ts = Node('ts', 4)
+node_103 = Node('103', 3)
+node_b3 = Node('b3', 3)
+node_b1 = Node('b1', 2)
+node_c2 = Node('c2', 3)
+node_c1 = Node('c1', 7)
+node_c3 = Node('c3', 0)
+node_b2 = Node('b2', 2)
+node_b4 = Node('b4', 6)
+node_109 = Node('109', 3)
+node_111 = Node('111', 0)
+node_119 = Node('119', 6)
+node_storage = Node('storage', 0)
+node_123 = Node('123', 3)
+node_123r = Node('123r', 0)
+node_125 = Node('125', 0)
 
 g1 = Graph(
     [node_mail, node_ts, node_103, node_b3, node_b1, node_c2, node_c1, node_c3, node_b2, node_b4, node_109, node_111, node_119, node_storage, node_123, node_123r, node_125],
@@ -46,7 +46,8 @@ g1 = Graph(
 )
 
 path = g1.find_path(node_103, node_123r)
-print(path)
+print(path.get('distance'))
+print(g1.iterative_search(node_103, node_123r, 5))
 for node in path.get('path'):
     print(node[0].value)
 
